e3c3247574b8aaa9c3391207c44233da55afda34
thomasahdy <thomasahdy274@gmail.com>
2026-03-16 22:16:34 +0200

    folder structre

--- backend/mongez/src/app.module.ts
+++ backend/mongez/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from './config/app.config';
+import databaseConfig from './config/database.config';
+import { HealthModule } from './common/health/health.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig, databaseConfig],
+    }),
+    HealthModule,
+  ],
 })
 export class AppModule {}
