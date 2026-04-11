389de7cc9f9767c65317a6339c8349257fa6836c
BasmalaMohamedd <basmalamohamed934@gmail.com>
2026-04-11 13:21:48 +0200

    init commit

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex flex-row'>
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Home = () => {
+  return (
+    <>
+    <Navbar />
+    <h1>Hello world</h1>
+    </>
+  )
+}
+
+export default Home
