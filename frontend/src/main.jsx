373fd8b621f6a545bc2653b71b05298c20818198
BasmalaMohamedd <basmalamohamed934@gmail.com>
2026-06-21 16:02:10 +0300

    tanstack react query main.jsx setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,20 @@ import './index.css';
 import App from './App.jsx';
 import { Provider } from 'react-redux';
 import { store } from './store/store.js';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import ErrorBoundary from './components/ErrorBoundary';
 import './i18n';
-
+const queryClient = new QueryClient();
 createRoot(document.getElementById('root')).render(
-  <ErrorBoundary>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </ErrorBoundary>
+  <QueryClientProvider client={queryClient}>
+    <ErrorBoundary>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ErrorBoundary>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
+  
 );
